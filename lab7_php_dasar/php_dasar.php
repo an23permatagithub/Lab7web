70ae0c2c0f959fb7a2f3415023f75e0acc6bac62
23antinipermatasari <antini.permatasari@mhs.pelitabangsa.ac.id>
2022-06-04 12:49:09 +0700

    menambahkan folder baru

---
+++ lab7_php_dasar/php_dasar.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Dasar</title>
+</head>
+<body>
+    <h1>PHP Dasar</h1>
+    <?php
+        echo "Hello World";
+    ?>
+
+    <hr>
+
+    <h2>Menggunakan Variable</h2>
+    <?php 
+        $nim = "312010095"; 
+        $nama = 'Antini permatasari'; 
+        
+        echo "NIM : " . $nim . "<br>"; 
+        echo "Nama : $nama"; 
+    ?>
+</body>
+</html>
